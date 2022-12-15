3ef7d6e58b3efd12ff4acadfdbca45ec60ae6ae3
gulnarvma@outlook.com <gulnarvma@outlook.com>
2022-12-15 14:28:10 +0400

    second commit

--- yasss/Program.cs
+++ yasss/Program.cs
@@ -4,24 +4,16 @@ namespace yasss
 {
     class Program
     {
+        private static object age;
+
         static void Main(string[] args)
         {
-            int il1, il2, ay1, ay2, gun1, gun2;
+            int yas, il1, il2, ay1, ay2, gun1, gun2;
 
-            Console.Write("il1 ededini daxil et:");
-            il1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("il2 ededini daxil et:");
-            il2 = Convert.ToInt32(Console.ReadLine());
-            int age1 = il2 - il1;
-            Console.WriteLine($"{il2}-{il1}={age1}");
-            Console.Write("ay1 ededini daxil et:");
-            ay1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("ay2 ededini daxil et:");
-            ay2 = Convert.ToInt32(Console.ReadLine());
-            if (ay1 > ay2)
-            {
-                Console.WriteLine($"{age1}-1");
-            }
+
+
+            Console.Write("yasi daxil et:");
+            yas = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("gun1 ededini daxil et:");
             gun1 = Convert.ToInt32(Console.ReadLine());
@@ -29,8 +21,27 @@ namespace yasss
             gun2 = Convert.ToInt32(Console.ReadLine());
             if (gun1 > gun2)
             {
-                Console.WriteLine($"{age1}-1");
+                Console.WriteLine($"{age}-1");
             }
+
+
+            Console.Write("ay1 ededini daxil et:");
+            ay1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ay2 ededini daxil et:");
+            ay2 = Convert.ToInt32(Console.ReadLine());
+            if (ay1 > ay2)
+            {
+                Console.WriteLine($"{age}-1");
+            }
+
+
+            Console.Write("il1 ededini daxil et:");
+            il1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("il2 ededini daxil et:");
+            il2 = Convert.ToInt32(Console.ReadLine());
+            int age1 = il2 - il1;
+            Console.WriteLine($"{il2}-{il1}={age1}");
+
 
             if (age1 > 1)
             {
